48e149caede535e8f36bd963b31c7b317b5b1d95
Moritz Molch <moritz.molch@cern.ch>
2024-03-24 21:20:14 +0100

    Add a generic CMSSWCommand task

--- emb_trigger_validation/tasks/base.py
+++ emb_trigger_validation/tasks/base.py
@@ -1,11 +1,14 @@
+from abc import ABCMeta, abstractmethod
 from collections import OrderedDict
 import law
 import law.contrib.wlcg
-from law.util import iter_chunks
+from law.util import iter_chunks, readable_popen
 import luigi
 import os
+from typing import Any, Dict, List, Optional, Union
 
 from emb_trigger_validation.config_util import ConfigManager
+from emb_trigger_validation.cmssw import CMSSWSandbox
 
 
 class BaseTask(law.Task):
@@ -20,7 +23,7 @@ class BaseTask(law.Task):
 
     local_store_path = luigi.PathParameter(
         exists=False,
-        description="path to the local file store for task outputs; default: '{}".format(os.environ["ETV_STORE_PATH"]),
+        description="path to the local file store for task outputs; default: '{}".format(os.environ["ETV_LOCAL_STORE_PATH"]),
         default=os.environ["ETV_LOCAL_STORE_PATH"],
     )
 
@@ -78,8 +81,8 @@ class ConfigTask(BaseTask):
     )
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.config_inst = ConfigManager().load_config(os.path.join(os.environ["ETV_CONFIG_PATH"], "analysis", self.config))
+        super(ConfigTask, self).__init__(*args, **kwargs)
+        self.config_inst = ConfigManager().load_config(os.path.join(os.environ["ETV_CONFIG_PATH"], "analysis", self.config, "config.yaml"))
 
     def path(self, store, *parts, **kwargs) -> str:
         return os.path.join(store, self.version, self.__class__.__name__, self.config_inst.name, *parts)
@@ -103,7 +106,7 @@ class DatasetTask(ConfigTask):
     )
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(DatasetTask, self).__init__(*args, **kwargs)
         self.dataset_inst = self.config_inst.get_dataset(self.dataset)
 
     def create_branch_map(self):
@@ -117,3 +120,128 @@ class DatasetTask(ConfigTask):
 
     def path(self, store, *parts, **kwargs) -> str:
         return os.path.join(store, self.version, self.__class__.__name__, self.config_inst.name, self.dataset_inst.name, *parts)
+
+
+class CMSSWCommandTask(BaseTask, metaclass=ABCMeta):
+
+    cmssw_threads = luigi.IntParameter(
+        description="number of threads which are used to compile the CMSSW release; default: 1",
+        default=1,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(CMSSWCommandTask, self).__init__(*args, **kwargs)
+        self._sandbox = CMSSWSandbox(
+            self,
+            self.cmssw_parent_dir(),
+            self.cmssw_release(),
+            self.cmssw_arch(),
+            custom_packages_script=self.cmssw_custom_packages_script(),
+            threads=self.cmssw_threads,
+        )
+
+    @abstractmethod
+    def cmssw_parent_dir(self) -> str:
+        """
+        Path to the directory, where CMSSW is installed within.
+
+        Abstract method, that must be implemented by subclasses.
+
+        :returns: path that contains the CMSSW release
+        :rtype:   str
+        """
+        return NotImplemented
+
+    @abstractmethod
+    def cmssw_release(self) -> str:
+        """
+        Name of the CMSSW release.
+
+        Abstract method, that must be implemented by subclasses.
+
+        :returns: name of the CMSSW release
+        :rtype:   str
+        """
+        return NotImplemented
+
+    @abstractmethod
+    def cmssw_arch(self) -> str:
+        """
+        Architecture that the CMSSW release will be compiled for.
+
+        Abstract method, that must be implemented by subclasses.
+
+        :returns: architecture for compiling CMSSW
+        :rtype:   str
+        """
+        return NotImplemented
+
+    #def cmssw_threads(self) -> int:
+    #    """
+    #    Number of threads used for compiling the CMSSW release.
+    #
+    #    :returns: number of threads for compilation; default: 1
+    #    :rtype:   int
+    #    """
+    #    return 1
+
+    def cmssw_custom_packages_script(self) -> Union[str, None]:
+        """
+        Path to a custom script for installing additional packages in the CMSSW release.
+
+        The script is executed after the installation of the base release and the release is compiled once again
+        after having pulled the additional packages.
+
+        By default, this method returns `None`. Subclasses can change the implementation of this method, so that it
+        returns a path to a custom install script.
+
+        :returns: path to install script for custom CMSSW packages or `None` if no custom script is provided
+        :rtype:   str | None
+        """
+        return None
+
+    def run_command(self, cmd: List[str], popen_kwargs: Optional[Dict[str, Any]] = None):
+        """
+        Run a shell command in a CMSSW environment, which has been set up with `cmsenv`.
+
+        The command has to be passed as a list of of its parts, which are meant to belong together, so that the command
+        can be properly quoted by :py:class:`subprocess.Popen`.
+
+        The parameter `popen_kwargs` obtains a dictionary with values for keyword arguments of
+        :py:class:`subprocess.Popen`. For a full list of available options, look at 
+        https://docs.python.org/3/library/subprocess.html#popen-constructor. Note that the values `shell=False`,
+        `executable=None` and `env=env` (with `env` being the environment provided by the sandbox) are going to be
+        fixed regardless of the settings passed to `popen_kwargs`.
+
+        :param cmd: command that is being executed
+        :type cmd: List[str]
+
+        :param popen_kwargs: explicit values for keyword arguments of `Popen`, optional
+        :type popen_kwargs: Dict[str, Any] | None
+
+        :raises RuntimeError: when the command has a non-zero exit code
+        """
+
+        # get the environment
+        env = self._sandbox.get_env()
+
+        # fix some values of the keyword arguments of Popen
+        popen_kwargs = popen_kwargs or {}
+        popen_kwargs.update({
+            "shell": False,
+            "executable": None,
+            "env": env,
+        })
+
+        # run the command
+        with self.publish_step("run command {}".format(cmd)):
+            p, lines = readable_popen(cmd, **popen_kwargs)
+
+            while True:
+                for line in lines:
+                    self.publish_message(line)
+                if not p.poll() is None:
+                    break
+
+            if p.returncode != 0:
+                raise RuntimeError("command failed with exit code {}".format(p.returncode))
