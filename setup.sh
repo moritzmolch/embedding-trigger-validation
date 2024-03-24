28bc7283e4ea24246edb784470f32942496cd14a
Moritz Molch <moritz.molch@cern.ch>
2024-03-24 21:16:50 +0100

    Add some environment variables

--- setup.sh
+++ setup.sh
@@ -10,20 +10,22 @@ action () {
     export ETV_BASE_PATH="${this_dir}"
 
     # auxiliary variables for special paths
+    export ETV_CACHE_PATH="${ETV_BASE_PATH}/cache"
     export ETV_CONFIG_PATH="${ETV_BASE_PATH}/config"
-    export ETV_MODULES_PATH="${ETV_BASE_PATH}/modules"
     export ETV_JOBS_PATH="${ETV_BASE_PATH}/jobs"
     export ETV_LOCAL_STORE_PATH="${ETV_BASE_PATH}/store"
+    export ETV_MODULES_PATH="${ETV_BASE_PATH}/modules"
+    export ETV_SOFTWARE_PATH="${ETV_BASE_PATH}/software"
 
     # default values for the WLCG store location
     export ETV_WLCG_STORE_NAME="wlcg_fs_gridka"
     export ETV_WLCG_STORE_PATH="$(basename "${ETV_BASE_PATH}" )/store"
 
     # set up software stack
-    source "/cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-centos8-gcc11-opt/setup.sh"
+    source "/cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-centos7-gcc11-opt/setup.sh"
 
     # additional dependencies (law, order, scinum)
-    export PYTHONPATH="${ETV_MODULES_PATH}:${ETV_MODULES_PATH}/law:${ETV_MODULES_PATH}/order:${ETV_MODULES_PATH}/scinum:${PYTHONPATH}"
+    export PYTHONPATH="${ETV_BASE_PATH}:${ETV_MODULES_PATH}/law:${ETV_MODULES_PATH}/order:${ETV_MODULES_PATH}/scinum:${PYTHONPATH}"
     export PATH="${ETV_MODULES_PATH}/law/bin:${PATH}"
 
     # configuration and output paths for law
