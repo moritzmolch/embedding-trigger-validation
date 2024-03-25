179cce41f41be6c6c75a827cdd98a4aa788cb7f5
Moritz Molch <moritz.molch@cern.ch>
2024-03-25 16:27:10 +0100

    Fix default settings for parameters in ntuple task

--- emb_trigger_validation/tasks/ntuples.py
+++ emb_trigger_validation/tasks/ntuples.py
@@ -2,19 +2,32 @@ import law
 from law.util import create_hash
 import luigi
 import os
-from typing import Union
+from order import Dataset, Process
+from typing import List, Union
 
-from emb_trigger_validation.tasks.base import CMSSWCommandTask, DatasetTask
+from emb_trigger_validation.tasks.base import ConfigTask, CMSSWCommandTask, DatasetTask
 from emb_trigger_validation.tasks.remote import BaseHTCondorWorkflow
 from emb_trigger_validation.tasks.bundle import BundleCMSSW
 
 
-class ProduceTauTriggerNtuples(CMSSWCommandTask, DatasetTask, law.LocalWorkflow, BaseHTCondorWorkflow):
+class ProduceTauTriggerNtuples(CMSSWCommandTask, DatasetTask, BaseHTCondorWorkflow, law.LocalWorkflow):
 
     threads = luigi.IntParameter(
-        description="number of threads used for executing the cmsRun command; default: 1",
-        default=1,
+        description="number of threads used for executing the cmsRun command; default: 4",
+        default=4,
     )
+
+    # hard-code the file group size for now
+    file_group_size = 10
+
+    @classmethod
+    def modify_param_values(cls, params):
+        # align number of requested CPUs to number of threads that are assigned for the command execution
+        params.update({
+            "htcondor_request_cpus": params.get("threads", 1)
+        })
+
+        return params
 
     def create_branch_map(self):
         # TODO allow flexible base URI, fixed for now
@@ -46,6 +59,9 @@ class ProduceTauTriggerNtuples(CMSSWCommandTask, DatasetTask, law.LocalWorkflow,
         reqs = super(ProduceTauTriggerNtuples, self).htcondor_workflow_requires()
         reqs["BundleCMSSW"] = BundleCMSSW.req(self, cmssw_path=self.cmssw_path())
         return reqs
+
+    def modify_polling_status_line(self, status_line):
+        return "{} - dataset: {}".format(status_line, law.util.colored(self.dataset_inst.name, color='light_cyan'))
 
     def cmssw_parent_dir(self) -> str:
         cmssw_hash = create_hash((self.cmssw_release(), self.cmssw_arch(), self.cmssw_custom_packages_script()))
@@ -118,3 +134,46 @@ class ProduceTauTriggerNtuples(CMSSWCommandTask, DatasetTask, law.LocalWorkflow,
         if not self.output().parent.exists():
             self.output().parent.touch()
         output.copy_from_local(tmp_ntuple_target)
+
+
+class ProduceTauTriggerNtuplesWrapper(ConfigTask, law.WrapperTask):
+
+    threads = luigi.IntParameter(
+        description="number of threads used for executing the cmsRun command; default: 1",
+        default=1,
+    )
+
+    root_process = luigi.Parameter(
+        description=(
+            "selection of the root process for processing datasets collectively; only datasets with a process, which "
+            "is a child of the given root process, are taken into account for constructing the requirements of this "
+            "wrapper task"
+        ),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(ProduceTauTriggerNtuplesWrapper, self).__init__(*args, **kwargs)
+        self.process_inst = self.config_inst.get_process(self.root_process)
+
+    def get_datasets_from_root_process(self, root_process: Process) -> List[Dataset]:
+        datasets = []
+        for dataset in self.config_inst.datasets.values():
+            if len(dataset.processes) == 0:
+                continue
+            dataset_root_process = dataset.processes.get_first().get_root_processes()[0]
+            if dataset_root_process == root_process:
+                datasets.append(dataset)
+        return datasets
+
+    def requires(self):
+        reqs = []
+        self.publish_message("wrapping tasks for datasets associated with the root process '{}'".format(self.process_inst.name))
+        for dataset in self.get_datasets_from_root_process(self.process_inst):
+            self.publish_message("adding dataset '{}' to wrapper".format(dataset.name))
+            reqs.append(
+                ProduceTauTriggerNtuples.req(
+                    self,
+                    dataset=dataset.name,
+                )
+            )
+        return reqs
