10ca1181b76b2b1d7b031c4aeccfdd3d21a8af52
Moritz Molch <moritz.molch@cern.ch>
2024-04-25 15:43:55 +0200

    Add config store extraction to boostrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -15,11 +15,13 @@ bootstrap_htcondor () {
 
     # set project variables that are needed before sourcing the environment
     export ETV_REMOTE="1"
-    export ETV_BASE_PATH="${LAW_JOB_HOME}/{{etv_repo_name}}"
+    export ETV_BASE="${LAW_JOB_HOME}/{{etv_repo_name}}"
 
     # patterns and URIs for external bundles that have to be pulled and unpacked
     export ETV_REPO_URIS="{{etv_repo_uris}}"
     export ETV_REPO_PATTERN="{{etv_repo_pattern}}"
+    export ETV_CONFIG_DATA_URIS="{{etv_config_data_uris}}"
+    export ETV_CONFIG_DATA_PATTERN="{{etv_config_data_pattern}}"
     export ETV_CMSSW_URIS="{{etv_cmssw_uris}}"
     export ETV_CMSSW_PATTERN="{{etv_cmssw_pattern}}"
 
@@ -29,8 +31,8 @@ bootstrap_htcondor () {
 
     # unpack the analysis repository in a sub-shell
     (
-        mkdir -p "${ETV_BASE_PATH}" &&
-        cd "${ETV_BASE_PATH}" &&
+        mkdir -p "${ETV_BASE}" &&
+        cd "${ETV_BASE}" &&
         source "${grid_wn_script}" "" &&
         source "${wlcg_tools_script}" "" &&
         law_wlcg_get_file "${ETV_REPO_URIS}" "${ETV_REPO_PATTERN}" "repo.tgz" &&
@@ -38,8 +40,19 @@ bootstrap_htcondor () {
         rm "repo.tgz"
     ) || return "$?"
  
+    # unpack the config data a sub-shell
+    (
+        mkdir -p "${ETV_BASE}/data/config" &&
+        cd "${ETV_BASE}/data/config" &&
+        source "${grid_wn_script}" "" &&
+        source "${wlcg_tools_script}" "" &&
+        law_wlcg_get_file "${ETV_CONFIG_DATA_URIS}" "${ETV_CONFIG_DATA_PATTERN}" "config_data.tgz" &&
+        tar -xzf "config_data.tgz" &&
+        rm "config_data.tgz"
+    ) || return "$?"
+ 
     # source the default repo environment
-    source "${ETV_BASE_PATH}/setup.sh" "" || return "$?"
+    source "${ETV_BASE}/setup.sh" "" || return "$?"
 
     export TMPDIR="${ORIG_CONDOR_TMPDIR}"
 
